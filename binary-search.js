e33660c1027d6c02e4efc5b5ec9a99ec0f8db6f9
Roberto Augusto <roberto.augusto@krooze.com.br>
2024-11-23 16:40:19 -0300

    Improving binary search logic

--- binary-search.js
+++ binary-search.js
@@ -1,13 +1,19 @@
 function binarySearch(list, search) {
+  const newList = Array.from(new Set(list.sort((a, b) => a - b))); // Remove duplicate and sort
+
   let start = 0;
-  let end = list.length - 1;
+  let end = newList.length - 1;
   let attempt = 0;
   
   while (start <= end) {
     attempt += 1;
 
     let currentMiddle = Math.floor((start + end) / 2);
-    let guess = list[currentMiddle];
+    let guess = newList[currentMiddle];
+
+    console.log(list.sort((a, b) => a - b));
+    console.log(newList);
+    console.log(guess);
 
     if (guess === search) {
       console.log(`Attempt: ${attempt}`);
@@ -22,3 +28,6 @@ function binarySearch(list, search) {
   console.log(`Attempt: ${attempt}`);
   return -1;
 }
+
+const list = [1,2,5,7,4,3,8,9,10,2,6];
+console.log(binarySearch`${list, 2}`);
